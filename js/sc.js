bb8a6fb610d79d2c78a2b9ba1e5755ae343f496a
PEPIISCUL <pepimazon@gmail.com>
2024-05-14 23:14:39 +0200

    e

--- js/sc.js
+++ js/sc.js
@@ -52,31 +52,36 @@ input.addEventListener('keydown', (event) => {
           document.getElementById(i + 1).innerHTML = a[2].bold()
           document.getElementById(i + 1).href = "html/cul.html"
           document.getElementById(i + 1 + "i").src = "img/3s.png"
-          document.getElementById("none1").innerHTML = ""
+          document.getElementById("none1").innerHTML = "SEARCH!!!".bold()
+          document.getElementById("none1").style = "font-size: 75px;text-align: center;font-family:'Courier New', Courier, monospace;"
         }
         if (aa[i][1] == "p"){
           document.getElementById(i + 1).innerHTML = a[0].bold()
           document.getElementById(i + 1).href = "PEPIMAZON/PepiMazon.html"
           document.getElementById(i + 1 + "i").src = "img/2s.png"
-          document.getElementById("none1").innerHTML = ""
+          document.getElementById("none1").innerHTML = "SEARCH!!!".bold()
+          document.getElementById("none1").style = "font-size: 75px;text-align: center;font-family:'Courier New', Courier, monospace;"
         }
         if (aa[i][1] == "c"){
           document.getElementById(i + 1).innerHTML = a[1].bold()
           document.getElementById(i + 1).href = "html/car.html"
           document.getElementById(i + 1 + "i").src = "img/1s.png"
           //console.log(i + 1 + "a")
-          document.getElementById("none1").innerHTML = ""
+          document.getElementById("none1").innerHTML = "SEARCH!!!".bold()
+          document.getElementById("none1").style = "font-size: 75px;text-align: center;font-family:'Courier New', Courier, monospace;"
         }
         }
       if (document.getElementById("e").value == "" || p == 0 && c == 0 && am == 0){
         if (document.getElementById("e").value != "" || p == 0 && c == 0 && am == 0){
-          document.getElementById("none1").innerHTML = "No results"
+          document.getElementById("none1").innerHTML = "No results".bold()
+          document.getElementById("none1").style = "font-size: 75px;text-align: center;font-family:'Courier New', Courier, monospace;"
           document.getElementById("1").innerHTML = ""
           document.getElementById("2").innerHTML = ""
           document.getElementById("3").innerHTML = ""
         }
         if (document.getElementById("e").value == ""){
-          document.getElementById("none1").innerHTML = ""
+          document.getElementById("none1").innerHTML = "SEARCH!!!".bold()
+          document.getElementById("none1").style = "font-size: 75px;text-align: center;font-family:'Courier New', Courier, monospace;"
           document.getElementById("1").innerHTML = ""
           document.getElementById("2").innerHTML = ""
           document.getElementById("3").innerHTML = ""
