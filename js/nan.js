ed9e3375feb2606b096f3433cb8e68c2ac5f85f9
PEPIISCUL <pepimazon@gmail.com>
2025-03-07 22:12:19 +0100

    nanobot

--- js/nan.js
+++ js/nan.js
@@ -6,6 +6,8 @@ var cx = document.getElementById("a").style.marginLeft
 var cy = document.getElementById("a").style.marginTop
 const screenHeight = window.screen.height - 200
 const screenWidth = window.screen.width - 150
+var count = 0
+var audio2 = new Audio('../audio/mmms.mp3')
 //function getRandomInt(max) {
 //  return Math.floor(Math.random() * max);
 //}
@@ -16,6 +18,22 @@ const screenWidth = window.screen.width - 150
 //document.getElementById("a").style.marginLeft = cx
 //document.getElementById("a").style.marginTop = cy
 function animate() {
+  if (localStorage.getItem("num2") == 1){
+    audio2.cloneNode(true).play();
+    localStorage.setItem("num2",0);
+  }
+  count = count + 1
+  if (count == 15){
+    for (let i = 0; i < parseInt(document.getElementById("nm").innerHTML); i++){
+      imherer = Math.floor(Math.random()*500)
+      if (imherer == 0){
+          audio2.cloneNode(true).play();
+      }
+      count = 0
+    }
+    
+  }
+  
   //document.getElementById("2r").innerHTML = cy.replace(/px/,"") + "         " + screenHeight
   if (cx.replace(/px/,"") > screenWidth || cx.replace(/px/,"") < 0) {
     x *= -1
--- js/nan2.js
+++ js/nan2.js
@@ -1,12 +1,50 @@
 var h = window.screen.availHeight - 350
 var w = window.screen.availWidth - 200
-var audio = new Audio('../audio/mmm.mp3')
+var audio = new Audio('../audio/mmms.mp3')
+var audio2 = new Audio('../audio/mmms.mp3')
+
 var num = 0
 var numero = 0
+function start(){
+    if (localStorage.getItem("num") != 0){
+
+        for (let i = 0; i < localStorage.getItem("num"); i++) {
+            num += 1
+            numero = Math.floor(Math.random()*4)
+            const newimg = document.createElement('img')
+            //document.getElementById("2r").innerHTML = w + ' ' + h
+            if (num != 1000) {
+                if (numero == 0) {
+                    newimg.src = "../img/anim.gif"
+                }
+                if (numero == 1) {
+                    newimg.src = "../img/anim2.gif"
+                }
+                if (numero == 2) {
+                    newimg.src = "../img/nanobotog.png"
+                }
+                if (numero == 3) {
+                    newimg.src = "../img/anim3.gif"
+                }
+            }
+            if (num == 1000) {
+                document.getElementById("a").style = "margin-top: 0px;margin-left: 0px;position: absolute;display:flex"
+            }
+            newimg.style = "position: absolute"
+            newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
+            newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
+            document.getElementById('z').appendChild(newimg)
+            document.getElementById('nm').innerHTML = num
+        }
+    }
+}
+
 
 function cul() {
-    audio.play()
+    audio.cloneNode(true).play();
+    //num = Math.floor(Math.random() * 10);
     num += 1
+
     numero = Math.floor(Math.random()*4)
     const newimg = document.createElement('img')
     //document.getElementById("2r").innerHTML = w + ' ' + h
@@ -31,10 +69,17 @@ function cul() {
     newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
     newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
     document.getElementById('z').appendChild(newimg)
-    document.getElementById('nm').innerHTML = 'Nanobots ordered: ' + num
+    document.getElementById('nm').innerHTML = num
+    localStorage.setItem("num",num);
+    if (num==1){
+        location.reload()
+        localStorage.setItem("num2",1);
+    }
 }
 
 function fun() {
-    document.getElementById('but').remove()
+    //document.getElementById('but').remove()
+    localStorage.setItem("num",0);
+    location.reload()
 }
 
