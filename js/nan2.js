b5bcc1debd47408b049b75758b926a6a0246f32f
PEPIISCUL <pepimazon@gmail.com>
2025-12-13 17:55:01 +0100

    sas

--- js/nan2.js
+++ js/nan2.js
@@ -9,12 +9,13 @@ var money = 75
 var jum = 0
 var numerogold = 0
 var numgold = 0
+var numexp1 = 0
 
 
 
-
-
-
+const nanos = ["numgold", "num2x", "numsmall", "numbig"];
+const nanostext = ["../img/nanobotoggold.png", "../img/nanobotog2X.png", "../img/nanobotogsmall.png", "../img/nanobotogbig.png"]
+var multi = 0;
 
 
 
@@ -27,7 +28,7 @@ function start(){
     document.getElementById('momy').innerHTML = "Money: " + money.toString() + "$"
     if (localStorage.getItem("num") != 0){
 
-        for (let i = 0; i < localStorage.getItem("num")-localStorage.getItem("numgold"); i++) {
+        for (let i = 0; i < localStorage.getItem("num")-localStorage.getItem("numgold")-localStorage.getItem("num2x")-localStorage.getItem("numsmall")-localStorage.getItem("numbig"); i++) {
             num += 1
             numero = Math.floor(Math.random()*4)
             const newimg = document.createElement('img')
@@ -53,22 +54,31 @@ function start(){
             newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
             newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
             document.getElementById('z').appendChild(newimg)
-            document.getElementById('nm').innerHTML = "Nanobots ordered: " + num
+            document.getElementById('nm').innerHTML = "Nanobots: " + num
             document.getElementById('nm2').innerHTML = num
         }
-        for (let i = 0; i < localStorage.getItem("numgold"); i++) {
-            num += 1
-            const newimg = document.createElement('img')
-            newimg.src = "../img/nanobotoggold.png"
-            newimg.setAttribute("onclick", "gold()")
-            numgold+=1
-            newimg.style = "position: absolute"
-            newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
-            newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
-            document.getElementById('z').appendChild(newimg)
-            document.getElementById('nm').innerHTML = "Nanobots ordered: " + num
-            document.getElementById('nm2').innerHTML = num
+        for (let j = 0; j < nanos.length; j++) {
+            for (let i = 0; i < localStorage.getItem(nanos[j]); i++) {
+                num += 1
+                const newimg = document.createElement('img')
+                newimg.src = nanostext[j]
+                
+                if (nanos[j] == "numgold"){
+                    newimg.setAttribute("onclick", "gold()")
+                    numgold+=1
+                }
+                
+                newimg.style = "position: absolute"
+                newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
+                newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
+                document.getElementById('z').appendChild(newimg)
+                document.getElementById('nm').innerHTML = "Nanobots: " + num
+                document.getElementById('nm2').innerHTML = num
+            }
         }
+        
+        
+
     }
 }
 
@@ -89,11 +99,13 @@ function cul() {
 
         numero = Math.floor(Math.random()*4)
         numerogold = Math.floor(Math.random()*1000)
-        
+        numexp1 = Math.floor(Math.random()*4)
+
         const newimg = document.createElement('img')
         
         //document.getElementById("2r").innerHTML = w + ' ' + h
         if (num != 1000) {
+            
             
             if (numerogold == 0){
                 newimg.src = "../img/nanobotoggold.png"
@@ -102,17 +114,31 @@ function cul() {
             }
             
             if (numerogold != 0){
-                if (numero == 0) {
-                    newimg.src = "../img/anim.gif"
+                if (numexp1 == 0){
+                    if (numero == 0) {
+                        newimg.src = "../img/anim.gif"
+                    }
+                    if (numero == 1) {
+                        newimg.src = "../img/anim2.gif"
+                    }
+                    if (numero == 2) {
+                        newimg.src = "../img/nanobotog.png"
+                    }
+                    if (numero == 3) {
+                        newimg.src = "../img/anim3.gif"
+                    }
                 }
-                if (numero == 1) {
-                    newimg.src = "../img/anim2.gif"
+                if (numexp1 == 1){
+                    newimg.src = "../img/nanobotog2X.png"
+                    localStorage.setItem("num2x", parseInt(localStorage.getItem("num2x"))+1);
                 }
-                if (numero == 2) {
-                    newimg.src = "../img/nanobotog.png"
+                if (numexp1 == 2){
+                    newimg.src = "../img/nanobotogsmall.png"
+                    localStorage.setItem("numsmall", parseInt(localStorage.getItem("numsmall"))+1);
                 }
-                if (numero == 3) {
-                    newimg.src = "../img/anim3.gif"
+                if (numexp1 == 3){
+                    newimg.src = "../img/nanobotogbig.png"
+                    localStorage.setItem("numbig", parseInt(localStorage.getItem("numbig"))+1);
                 }
             }
             
@@ -126,7 +152,7 @@ function cul() {
         newimg.style.marginLeft = Math.floor(Math.random()*w) + 'px'
         newimg.style.marginTop = Math.floor(Math.random()*h) + 'px'
         document.getElementById('z').appendChild(newimg)
-        document.getElementById('nm').innerHTML = "Nanobots ordered: " + num.toString()
+        document.getElementById('nm').innerHTML = "Nanobots: " + num.toString()
         document.getElementById('nm2').innerHTML = num
         document.getElementById('momy').innerHTML = "Money: " + money.toString() + "$"
         localStorage.setItem("num",num);
@@ -144,6 +170,9 @@ function fun() {
     localStorage.setItem("num",0);
     localStorage.setItem("mum",75);
     localStorage.setItem("numgold",0);
+    localStorage.setItem("num2x",0);
+    localStorage.setItem("numsmall",0);
+    localStorage.setItem("numbig",0);
     localStorage.setItem("trashcan", "0")
     localStorage.setItem("moneybuy", "0")
     localStorage.setItem("almanac", "0")
